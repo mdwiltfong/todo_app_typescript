7589c48ee8574c2c718b5f8df9bce40c35284b22
mdwiltfong <wiltfong.michael1@gmail.com>
2022-07-20 21:48:27 -0400

    added bootstrap

--- dist/app.js
+++ dist/app.js
@@ -1,13 +1,14 @@
-var ProjectInput = /** @class */ (function () {
-    function ProjectInput() {
+"use strict";
+class ProjectInput {
+    constructor() {
         this.root = document.getElementById("root");
         this.attach();
     }
-    ProjectInput.prototype.attach = function () {
-        var div = document.createElement("div");
+    attach() {
+        const div = document.createElement("div");
         div.setAttribute("id", "container");
         this.root.appendChild(div);
-    };
-    return ProjectInput;
-}());
+    }
+}
 new ProjectInput();
+//# sourceMappingURL=app.js.map
